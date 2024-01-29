20f0bd0d475a0d816a120b429f054c4d83cbdf73
Винокуров Савелий <savinokurov@sbercloud.ru>
2024-01-29 20:46:40 +0300

    cancellation

---
+++ cancellation/cancellation.go
@@ -0,0 +1,64 @@
+// use 'done' channel to signal goroutine to stop, by closing 'done'.
+// after closing it returns zero value immediately and breaks the pipeline
+// before all values have been recieved
+package cancellation
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	// Set up a done channel that's shared by the whole pipeline,
+	// and close that channel when this pipeline exits, as a signal
+	// for all the goroutines we started to exit.
+	done := make(chan struct{})
+	defer close(done)
+
+	// generate sequence
+	in := gen(done, 2, 3)
+
+	// Distribute the sq work across two goroutines that both read from in.
+	c1 := sq(done, in)
+	c2 := sq(done, in)
+
+	// Consume the first value from output.
+	out := merge(done, c1, c2)
+	fmt.Println(<-out) // 4 or 9
+
+	// done will be closed by the deferred call.
+}
+
+// Each of our pipeline stages is now free to return as soon as done is closed.
+// The output routine in merge can return without draining its inbound channel,
+// since it knows the upstream sender, sq, will stop attempting to send when done is closed.
+func merge(done <-chan struct{}, cs ...<-chan int) <-chan int {
+	var wg sync.WaitGroup
+	out := make(chan int)
+
+	// Start an output goroutine for each input channel in cs.  output
+	// copies values from c to out until c or done is closed, then calls
+	// wg.Done.
+	output := func(c <-chan int) {
+		defer wg.Done()
+		for n := range c {
+			select {
+			case out <- n:
+			case <-done:
+				return
+			}
+		}
+	}
+	wg.Add(len(cs))
+	for _, c := range cs {
+		go output(c)
+	}
+
+	// Start a goroutine to close out once all the output goroutines are
+	// done.  This must start after the wg.Add call.
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
+}
